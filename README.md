e76e666952eca6fc14523b3b6ef99c63ae4babfc
Robert DeSimone <robert.desimone@auth0.com>
2020-12-15 12:21:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### console.yulelog 
+
+Adds some holiday festiveness to your console output!
+
+To use:
+
+```
+require('./dist/main');
+
+console.yulelog('Happy holidays!');
+```
---
+++ src/index.js
@@ -0,0 +1,10 @@
+const yulelog = function() {
+  console.yulelog = function(...args) {
+    const [msgOrObject, ...rest] = args;
+    console.log(`🎄☃️🔥 ${msgOrObject}`, ...rest);
+  }
+}
+
+module.exports = yulelog;
+
+yulelog();
